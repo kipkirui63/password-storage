102757a4036784f2d738c7e33c271e51cccd60db
kipkirui63 <enocksang8356@gmail.com>
2024-04-21 18:13:40 +0300

    initialized flask app

---
+++ server/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
+from flask_restx import Api, Namespace, Resource
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///manager.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+
+CORS(app)
+
+
+db=SQLAlchemy(app)
+db.init_app(app)
+migrate=Migrate(app,db)
+ma =  Marshmallow(app)
+server = Api(app)
+
+from server import routes , models
+
+
+
+
